e1bb6f8cadb62233f8d6ee7aeacf6ed4991ca52b
ThangaPri <priya.thangam2023@gmail.com>
2024-09-04 02:23:51 -0400

    Checking and changing  code for syntactical and stylistic issues

--- service/routes.py
+++ service/routes.py
@@ -32,7 +32,7 @@ def index():
             # paths=url_for("list_accounts", _external=True),
         ),
         status.HTTP_200_OK,
-    )
+        )
 
 
 ######################################################################
@@ -60,12 +60,11 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 # ... place you code here to LIST accounts ...
-@app.route("/accounts", methods=["GET"])
-        
-def list_accounts():
 
+
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
     """
     List all Accounts
     This endpoint will list all Accounts
@@ -79,22 +78,23 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 # ... place you code here to READ an account ...
-@app.route("/accounts/<int:account_id>",methods=["GET"])
-def read_accounts(account_id):
-    app.logger.info("Request to read an account with id:%s",account_id)
 
-    account=Account.find(account_id)
+
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_accounts(account_id):
+    app.logger.info("Request to read an account with id: %s", account_id)
+
+    account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND,f"Account with id [{account_id}] could not be found")
-    return account.serialize(),status.HTTP_200_OK
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -112,8 +112,9 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 # ... place you code here to DELETE an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -141,4 +142,3 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
-
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -127,11 +127,11 @@ class TestAccountService(TestCase):
     def test_read_an_Account(self):
         account = self._create_accounts(1)[0]
         resp = self.client.get(
-            f"{BASE_URL}/{account.id}",content_type="application/json"
+            f"{BASE_URL}/{account.id}", content_type="application/json"
         )
-        self.assertEqual(resp.status_code,status.HTTP_200_OK)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(data["name"],account.name)
+        self.assertEqual(data["name"], account.name)
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
@@ -161,16 +161,12 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-   
